8f633fb4f882d6c2576da6ced1141755789e8bf0
Jonathan Peppers <jonathan.peppers@microsoft.com>
2018-07-08 10:38:52 -0500

    File | New Project
    Implemented my first thoughts on loading Application type

---
+++ Xamarin.Android.Lite/MainActivity.cs
@@ -0,0 +1,43 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Android.Util;
+using System;
+
+namespace Xamarin.Android.Lite
+{
+	[Activity (Label = "Xamarin.Android.Lite", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : Forms.Platform.Android.FormsAppCompatActivity
+	{
+		const string Tag = "XALite";
+		const string Metadata = "Xamarin.Android.Lite.Application";
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			TabLayoutResource = Resource.Layout.Tabbar;
+			ToolbarResource = Resource.Layout.Toolbar;
+
+			base.OnCreate (bundle);
+
+			Forms.Forms.Init (this, bundle);
+
+			var applicationInfo = PackageManager.GetApplicationInfo (PackageName, PackageInfoFlags.MetaData);
+			var applicationType = applicationInfo.MetaData.GetString (Metadata);
+			if (!string.IsNullOrEmpty (applicationType)) {
+				var type = Type.GetType (Metadata);
+				if (type != null) {
+					try {
+						LoadApplication ((Forms.Application)Activator.CreateInstance (type));
+					} catch (InvalidCastException) {
+						Log.Error (Tag, "Unable to cast type `{0}` from metadata to Xamarin.Forms.Application!", type);
+					}
+				} else {
+					Log.Error (Tag, "Unable to create type `{0}` from metadata in AndroidManifest.xml!", applicationType);
+				}
+			} else {
+				Log.Error (Tag, "Unable to find `{0}` metadata in AndroidManifest.xml!", Metadata);
+			}
+		}
+	}
+}
+
---
+++ Xamarin.Android.Lite/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Android.App;
+
+[assembly: AssemblyTitle("Xamarin.Android.Lite")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Xamarin.Android.Lite")]
+[assembly: AssemblyCopyright("Copyright© Jon Peppers 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: UsesPermission(Android.Manifest.Permission.Internet)]
+[assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage)]
