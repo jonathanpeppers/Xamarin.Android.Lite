d67b3c2d703e765868add101ff1f8cb6b91d06ff
Jonathan Peppers <jonathan.peppers@microsoft.com>
2018-07-10 14:00:40 -0500

    WIP LinkAssemblies
    Still getting crash on startup:
    
    07-10 15:00:25.065 16413-16413/? E/AndroidRuntime: FATAL EXCEPTION: main
        Process: com.xamarin.android.lite, PID: 16413
        android.runtime.JavaProxyThrowable: System.Reflection.TargetInvocationException: Exception has been thrown by the target of an invocation. ---> System.TypeLoadException: Could not resolve type with token 01000022 (from typeref, class/assembly System.Xml.IXmlNamespaceResolver, mscorlib, Version=2.0.5.0, Culture=neutral, PublicKeyToken=null)
          at Xamarin.Android.Lite.Sample.App..ctor () [0x00008] in <9164dc8bcdce4d7da6c681bebfc4a53a>:0
            at (wrapper managed-to-native) System.Reflection.MonoCMethod.InternalInvoke(System.Reflection.MonoCMethod,object,object[],System.Exception&)
          at System.Reflection.MonoCMethod.InternalInvoke (System.Object obj, System.Object[] parameters) [0x00002] in <70285d84d42a420ca486e47949ff5958>:0
           --- End of inner exception stack trace ---
          at System.Reflection.MonoCMethod.InternalInvoke (System.Object obj, System.Object[] parameters) [0x00017] in <70285d84d42a420ca486e47949ff5958>:0
          at System.RuntimeType.CreateInstanceMono (System.Boolean nonPublic) [0x000a8] in <70285d84d42a420ca486e47949ff5958>:0
          at System.RuntimeType.CreateInstanceSlow (System.Boolean publicOnly, System.Boolean skipCheckThis, System.Boolean fillCache, System.Threading.StackCrawlMark& stackMark) [0x00009] in <70285d84d42a420ca486e47949ff5958>:0
          at System.RuntimeType.CreateInstanceDefaultCtor (System.Boolean publicOnly, System.Boolean skipCheckThis, System.Boolean fillCache, System.Threading.StackCrawlMark& stackMark) [0x00027] in <70285d84d42a420ca486e47949ff5958>:0
          at System.Activator.CreateInstance (System.Type type, System.Boolean nonPublic) [0x00020] in <70285d84d42a420ca486e47949ff5958>:0
          at System.Activator.CreateInstance (System.Type type) [0x00000] in <70285d84d42a420ca486e47949ff5958>:0
          at Xamarin.Android.Lite.MainActivity.OnCreate (Android.OS.Bundle bundle) [0x00075] in <801fd3c3f1594794bce407ac64142967>:0
          at Android.App.Activity.n_OnCreate_Landroid_os_Bundle_ (System.IntPtr jnienv, System.IntPtr native__this, System.IntPtr native_savedInstanceState) [0x00011] in <c93232c8ab4946dea4bda41b33459c08>:0
            at (wrapper dynamic-method) System.Object.7(intptr,intptr,intptr)
            at md5bff8b7c7908ce4fe5d805acf2300a9b4.MainActivity.n_onCreate(Native Method)
            at md5bff8b7c7908ce4fe5d805acf2300a9b4.MainActivity.onCreate(MainActivity.java:29)
            at android.app.Activity.performCreate(Activity.java:7009)
            at android.app.Activity.performCreate(Activity.java:7000)
            at android.app.Instrumentation.callActivityOnCreate(Instrumentation.java:1214)
            at android.app.ActivityThread.performLaunchActivity(ActivityThread.java:2731)
            at android.app.ActivityThread.handleLaunchActivity(ActivityThread.java:2856)
            at android.app.ActivityThread.-wrap11(Unknown Source:0)
            at android.app.ActivityThread$H.handleMessage(ActivityThread.java:1589)
            at android.os.Handler.dispatchMessage(Handler.java:106)
            at android.os.Looper.loop(Looper.java:164)
            at android.app.ActivityThread.main(ActivityThread.java:6494)
            at java.lang.reflect.Method.invoke(Native Method)
            at com.android.internal.os.RuntimeInit$MethodAndArgsCaller.run(RuntimeInit.java:438)
            at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:807)

---
+++ Xamarin.Android.Lite.Tasks/LinkAssemblies.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using Mono.Cecil;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Xamarin.Android.Lite.Tasks
+{
+	/// <summary>
+	/// NOTE: this is not a "real" linker, it fixes up assemblies that reference netstandard.dll, making them instead point to mscorlib.dll
+	/// </summary>
+	public class LinkAssemblies : Task
+	{
+		[Required]
+		public string [] InputAssemblies { get; set; }
+
+		[Required]
+		public string [] OutputAssemblies { get; set; }
+
+		static readonly Dictionary<string, AssemblyRef> mapping = new Dictionary<string, AssemblyRef> {
+			{
+				"netstandard",
+				new AssemblyRef {
+					Name = "mscorlib",
+					Version = new Version(2, 0, 5, 0),
+				}
+			}
+		};
+
+		class AssemblyRef
+		{
+			public string Name { get; set; }
+
+			public Version Version { get; set; }
+
+			public AssemblyRef[] Children { get; set; }
+		}
+
+		public override bool Execute ()
+		{
+			using (var resolver = new DefaultAssemblyResolver ()) {
+				var rp = new ReaderParameters () {
+					AssemblyResolver = resolver,
+				};
+				string input, output;
+				for (int i = 0; i < InputAssemblies.Length; i++) {
+					input = InputAssemblies [i];
+					output = OutputAssemblies [i];
+
+					resolver.AddSearchDirectory (Path.GetDirectoryName (input));
+
+					var assembly = AssemblyDefinition.ReadAssembly (input, rp);
+					var references = assembly.MainModule.AssemblyReferences;
+					for (int j = 0; j < references.Count; j++) {
+						if (mapping.TryGetValue (references[j].Name, out AssemblyRef target)) {
+							references [j] = new AssemblyNameReference (target.Name, target.Version);
+						}
+					}
+
+					Directory.CreateDirectory (Path.GetDirectoryName (output));
+					assembly.Write (output);
+				}
+
+				return !Log.HasLoggedErrors;
+			}
+		}
+	}
+}
