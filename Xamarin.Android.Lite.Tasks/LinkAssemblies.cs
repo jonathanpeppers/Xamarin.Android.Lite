941cdcb1cbdd3ed7d3c48809d7bc32290787ce61
Jonathan Peppers <jonathan.peppers@microsoft.com>
2018-07-10 11:39:41 -0500

    Using full linker code
    Not so sure about it, it doesn't work...

---
+++ Xamarin.Android.Lite.Tasks/LinkAssemblies.cs
@@ -0,0 +1,86 @@
+﻿using Java.Interop.Tools.Cecil;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+using Mono.Linker;
+using Mono.Linker.Steps;
+using MonoDroid.Tuner;
+using System.IO;
+using MSBuild = Microsoft.Build.Framework;
+
+namespace Xamarin.Android.Lite.Tasks
+{
+	/// <summary>
+	/// NOTE: this is not a "real" linker, it fixes up assemblies that reference netstandard.dll, making them instead point to mscorlib.dll
+	/// </summary>
+	public class LinkAssemblies : Task, Mono.Linker.ILogger
+	{
+		[Required]
+		public string MainAssembly { get; set; }
+
+		public ITaskItem [] ResolvedAssemblies { get; set; }
+
+		[Required]
+		public string OutputDirectory { get; set; }
+
+		public override bool Execute ()
+		{
+			var rp = new ReaderParameters {
+				InMemory = true,
+			};
+			using (var res = new DirectoryAssemblyResolver (this.CreateTaskLogger (), loadDebugSymbols: false, loadReaderParameters: rp)) {
+
+				// Put every assembly we'll need in the resolver
+				res.Load (MainAssembly);
+				foreach (var assembly in ResolvedAssemblies) {
+					res.Load (Path.GetFullPath (assembly.ItemSpec));
+				}
+
+				var resolver = new AssemblyResolver (res.ToResolverCache ());
+				//resolver.AddSearchDirectory (OutputDirectory);
+				//if (ResolvedAssemblies != null) {
+				//	foreach (var assembly in ResolvedAssemblies) {
+				//		resolver.AddSearchDirectory (Path.GetDirectoryName (assembly.ItemSpec));
+				//	}
+				//}
+
+				var pipeline = new Pipeline ();
+				pipeline.AppendStep (new ResolveFromAssemblyStep (MainAssembly));
+				pipeline.AppendStep (new FixAbstractMethodsStep ());
+				pipeline.AppendStep (new OutputStep ());
+
+				var context = new AndroidLinkContext (pipeline, resolver) {
+					Logger = this,
+					CoreAction = AssemblyAction.Link,
+					UserAction = AssemblyAction.Link,
+					LinkSymbols = true,
+					SymbolReaderProvider = new DefaultSymbolReaderProvider (true),
+					SymbolWriterProvider = new DefaultSymbolWriterProvider (),
+					OutputDirectory = OutputDirectory
+				};
+
+				pipeline.Process (context);
+
+				return !Log.HasLoggedErrors;
+			}
+		}
+
+		public void LogMessage (Mono.Linker.MessageImportance importance, string message, params object [] values)
+		{
+			switch (importance) {
+				case Mono.Linker.MessageImportance.High:
+					Log.LogMessage (MSBuild.MessageImportance.High, message, values);
+					break;
+				case Mono.Linker.MessageImportance.Low:
+					Log.LogMessage (MSBuild.MessageImportance.Low, message, values);
+					break;
+				case Mono.Linker.MessageImportance.Normal:
+					Log.LogMessage (MSBuild.MessageImportance.Normal, message, values);
+					break;
+				default:
+					break;
+			}
+		}
+	}
+}
