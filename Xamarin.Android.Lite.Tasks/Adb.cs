5af631997cf561d331e76ab1b317d0486a99226a
Jonathan Peppers <jonathan.peppers@microsoft.com>
2018-07-11 10:32:25 -0500

    Implemented a simple Install target

---
+++ Xamarin.Android.Lite.Tasks/Adb.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using System;
+using System.IO;
+using Xamarin.Android.Tools;
+
+namespace Xamarin.Android.Lite.Tasks
+{
+	public class Adb : ToolTask
+	{
+		[Required]
+		public string Command { get; set; }
+
+		protected override string GenerateCommandLineCommands ()
+		{
+			return Command;
+		}
+
+		protected override void LogEventsFromTextOutput (string singleLine, MessageImportance messageImportance)
+		{
+			string text = singleLine.Trim ();
+
+			if (text.StartsWith ("adb: usage:", StringComparison.OrdinalIgnoreCase)) {
+				Log.LogError (text);
+				return;
+			}
+
+			base.LogEventsFromTextOutput (singleLine, messageImportance);
+		}
+
+		protected override string ToolName => OS.IsWindows ? "adb.exe" : "adb";
+
+		protected override string GenerateFullPathToTool () => Path.Combine (ToolPath, ToolExe);
+	}
+}
