d0322358ec0630f02665d8df9ae5e55f3db33116
Jonathan Peppers <jonathan.peppers@microsoft.com>
2018-07-11 21:55:30 -0500

    I don't know, want to come back to this

--- Xamarin.Android.Lite.Tasks/LinkAssemblies.cs
+++ Xamarin.Android.Lite.Tasks/LinkAssemblies.cs
@@ -1,23 +1,16 @@
-using Java.Interop.Tools.Cecil;
-using Microsoft.Build.Framework;
+﻿using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
 using Mono.Cecil;
-using Mono.Cecil.Cil;
-using Mono.Linker;
-using Mono.Linker.Steps;
-using MonoDroid.Tuner;
+using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using Xamarin.Android.Tasks;
-using MSBuild = Microsoft.Build.Framework;
 
 namespace Xamarin.Android.Lite.Tasks
 {
 	/// <summary>
 	/// NOTE: this is not a "real" linker, it fixes up assemblies that reference netstandard.dll, making them instead point to mscorlib.dll
 	/// </summary>
-	public class LinkAssemblies : Task, Mono.Linker.ILogger
+	public class LinkAssemblies : Task
 	{
 		[Required]
 		public string InputDirectory { get; set; }
@@ -25,76 +18,78 @@ namespace Xamarin.Android.Lite.Tasks
 		[Required]
 		public string OutputDirectory { get; set; }
 
+		static readonly Version DotNetVersion = new Version (2, 0, 5, 0);
+
+		/// <summary>
+		/// Mapping of referenced assemblies that need "fixed"
+		/// </summary>
+		static readonly Dictionary<string, AssemblyRef> mapping = new Dictionary<string, AssemblyRef> {
+			{
+				"netstandard",
+				new AssemblyRef {
+					Name = "mscorlib",
+					Alternates = new Dictionary<string, AssemblyRef> {
+						{ "System.Xml", new AssemblyRef { Name = "System.Xml" } }
+					}
+				}
+			}
+		};
+
+		class AssemblyRef
+		{
+			public string Name { get; set; }
+
+			public Version Version { get; set; } = DotNetVersion;
+
+			/// <summary>
+			/// References of namespaces that need "fixed"
+			/// </summary>
+			public Dictionary<string, AssemblyRef> Alternates { get; set; }
+		}
+
 		public override bool Execute ()
 		{
 			if (!Directory.Exists (InputDirectory)) {
-				Log.LogError ("{0} does not exist at path `{1}`", nameof (InputDirectory), InputDirectory);
+				Log.LogError ("Unable to find {0} at `{1}`", nameof (InputDirectory), InputDirectory);
 				return false;
 			}
 
-			var linkerDirectory = Path.Combine (Path.GetDirectoryName (GetType ().Assembly.Location), "linker");
-			if (!Directory.Exists (linkerDirectory)) {
-				Log.LogError ("Unable to find linker input assemblies at path `{0}`", linkerDirectory);
-				return false;
-			}
+			using (var resolver = new DefaultAssemblyResolver ()) {
+				var rp = new ReaderParameters {
+					InMemory = true,
+					AssemblyResolver = resolver,
+				};
+				foreach (var input in Directory.EnumerateFiles (InputDirectory, "*.dll")) {
+					resolver.AddSearchDirectory (Path.GetDirectoryName (input));
 
-			var rp = new ReaderParameters {
-				InMemory = true,
-			};
-			using (var res = new DirectoryAssemblyResolver (this.CreateTaskLogger (), loadDebugSymbols: false, loadReaderParameters: rp)) {
-				var linkerAssemblies = new List<string> ();
-				linkerAssemblies.AddRange (Directory.EnumerateFiles (InputDirectory, "*.dll"));
-				linkerAssemblies.AddRange (Directory.EnumerateFiles (linkerDirectory, "*.dll"));
-				foreach (var assembly in linkerAssemblies) {
-					res.Load (Path.GetFullPath (assembly));
-				}
-
-				using (var resolver = new AssemblyResolver (res.ToResolverCache ())) {
-					resolver.AddSearchDirectory (OutputDirectory);
-					foreach (var assembly in linkerAssemblies) {
-						resolver.AddSearchDirectory (Path.GetDirectoryName (assembly));
+					var assembly = AssemblyDefinition.ReadAssembly (input, rp);
+					var references = assembly.MainModule.AssemblyReferences;
+					AssemblyNameReference reference;
+					for (int j = 0; j < references.Count; j++) {
+						reference = references [j];
+						if (mapping.TryGetValue (reference.Name, out AssemblyRef target)) {
+							Log.LogMessage (MessageImportance.Low, "Remapping reference from `{0}` to `{1}", reference.Name, target.Name);
+							references [j] = new AssemblyNameReference (target.Name, target.Version);
+						}
 					}
 
-					var pipeline = new Pipeline ();
-					pipeline.AppendStep (new FixAbstractMethodsStep ());
-					pipeline.AppendStep (new OutputStep ());
-
-					foreach (var assembly in linkerAssemblies) {
-						pipeline.PrependStep (new ResolveFromAssemblyStep (assembly));
+					foreach (var typeReference in assembly.MainModule.GetTypeReferences ()) {
+						if (mapping.TryGetValue (typeReference.Scope.Name, out AssemblyRef target)) {
+							if (target.Alternates != null && target.Alternates.TryGetValue (typeReference.Namespace, out AssemblyRef alternate)) {
+								Log.LogMessage (MessageImportance.Low, "Remapping type from `{0}` to `{1}", typeReference.Scope.Name, alternate.Name);
+								typeReference.Scope = new AssemblyNameReference (alternate.Name, alternate.Version);
+							} else {
+								Log.LogMessage (MessageImportance.Low, "Remapping type from `{0}` to `{1}", typeReference.Scope.Name, target.Name);
+								typeReference.Scope = new AssemblyNameReference (target.Name, target.Version);
+							}
+						}
 					}
 
-					var context = new AndroidLinkContext (pipeline, resolver) {
-						LogMessages = true,
-						Logger = this,
-						CoreAction = AssemblyAction.Link,
-						UserAction = AssemblyAction.Link,
-						LinkSymbols = true,
-						SymbolReaderProvider = new DefaultSymbolReaderProvider (false),
-						SymbolWriterProvider = new DefaultSymbolWriterProvider (),
-						OutputDirectory = OutputDirectory
-					};
-
-					pipeline.Process (context);
+					Directory.CreateDirectory (OutputDirectory);
+					assembly.Write (Path.Combine (OutputDirectory, Path.GetFileName (input)));
 				}
 
 				return !Log.HasLoggedErrors;
-			}
-		}
-
-		public void LogMessage (Mono.Linker.MessageImportance importance, string message, params object [] values)
-		{
-			switch (importance) {
-				case Mono.Linker.MessageImportance.High:
-					Log.LogMessage (MSBuild.MessageImportance.High, message, values);
-					break;
-				case Mono.Linker.MessageImportance.Low:
-					Log.LogMessage (MSBuild.MessageImportance.Low, message, values);
-					break;
-				case Mono.Linker.MessageImportance.Normal:
-					Log.LogMessage (MSBuild.MessageImportance.Normal, message, values);
-					break;
-				default:
-					break;
 			}
 		}
 	}
