9ae11446b93610ff26494c8b321b9c748ca10d87
Jonathan Peppers <jonathan.peppers@microsoft.com>
2018-07-24 16:47:37 -0500

    Move tasks around, task to remove files from zip

---
+++ Xamarin.Android.Lite.Tasks/Tasks/GenerateManifest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace Xamarin.Android.Lite.Tasks
+{
+	public class GenerateManifest : Task
+	{
+		public string PackageName { get; set; }
+
+		public string VersionCode { get; set; }
+
+		public string VersionName { get; set; }
+
+		[Required]
+		public string AppClass { get; set; }
+
+		public override bool Execute ()
+		{
+
+			return !Log.HasLoggedErrors;
+		}
+	}
+}
---
+++ Xamarin.Android.Lite.Tasks/Tasks/RemoveFromApk.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using System;
+using System.IO;
+using Xamarin.Tools.Zip;
+
+namespace Xamarin.Android.Lite.Tasks
+{
+	public class RemoveFromApk : Task
+	{
+		[Required]
+		public string [] Files { get; set; }
+
+		[Required]
+		public ITaskItem Apk { get; set; }
+
+		public override bool Execute ()
+		{
+			if (Files.Length == 0) {
+				Log.LogError ("No files were specified to be removed from the APK!");
+				return false;
+			}
+
+			using (var zip = ZipArchive.Open (Apk.ItemSpec, FileMode.Create)) {
+				foreach (var file in Files) {
+					//NOTE: always use / on Android
+					var pathInZip = file.Replace (Path.DirectorySeparatorChar, '/');
+					if (zip.ContainsEntry (pathInZip)) {
+						Log.LogMessage (MessageImportance.Low, "Removing `{0}` from zip `{1}`", pathInZip, Apk.ItemSpec);
+						zip.DeleteEntry (pathInZip);
+					} else {
+						Log.LogWarning ("Cannot remove `{0}` from zip `{1}`, it was not found!", file, Apk.ItemSpec);
+					}
+				}
+			}
+
+			return !Log.HasLoggedErrors;
+		}
+
+		static CompressionMethod GetCompression (ITaskItem file)
+		{
+			if (Enum.TryParse (file.GetMetadata ("Compression"), out CompressionMethod compression))
+				return compression;
+
+			return CompressionMethod.Default;
+		}
+	}
+}
