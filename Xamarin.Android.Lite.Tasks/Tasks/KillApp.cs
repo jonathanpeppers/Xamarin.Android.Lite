f3f70d4b6dc7d378b2a4aea185b862f7fc8e7c17
Jonathan Peppers <jonathan.peppers@microsoft.com>
2018-07-26 11:18:42 -0500

    Fast Dev now kills the app
    - Moved `@(_FastDevDirectories)` creation to apk installation
    - Now running a `<KillApp />` task

---
+++ Xamarin.Android.Lite.Tasks/Tasks/KillApp.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Build.Framework;
+
+namespace Xamarin.Android.Lite.Tasks
+{
+	public class KillApp : BroadcastTask
+	{
+		[Required]
+		public string PackageName { get; set; }
+
+		const string IntentAction = "mono.android.intent.action.SEPPUKU";
+		const string Category = "mono.android.intent.category.SEPPUKU";
+		const string Data = "mono.android.Seppuku";
+
+		protected override string GenerateBroadcastCommand ()
+		{
+			return $"-a {IntentAction} -c {Category}.{PackageName} -n \"{PackageName}/{Data}\"";
+		}
+
+		protected override void OnData (string data)
+		{
+			//Don't need to do anything
+		}
+	}
+}
