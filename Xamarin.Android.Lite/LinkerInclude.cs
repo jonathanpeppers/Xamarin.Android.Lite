8124ec9055180eb6ff630ce6ed8fbc4da4063afe
Jonathan Peppers <jonathan.peppers@microsoft.com>
2019-06-17 22:52:30 -0500

    Fix linker crash on startup

---
+++ Xamarin.Android.Lite/LinkerInclude.cs
@@ -0,0 +1,12 @@
+﻿using FileProvider = global::Android.Support.V4.Content.FileProvider;
+
+namespace Xamarin.Android.Lite
+{
+	public class LinkerInclude
+	{
+		public void FileProvider (FileProvider provider)
+		{
+			provider = new FileProvider ();
+		}
+	}
+}
